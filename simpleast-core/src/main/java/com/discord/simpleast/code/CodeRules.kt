746fda8735e3946ec466e21d01d5a427dafbf41a
Tyviebrock <stevenrockss13@gmail.com>
2026-07-11 00:39:05 -0400

    Enhance CodeRules with additional file type support
    Added support for gyp, jsx, tsx, mts, and cts file types in CodeRules.

--- simpleast-core/src/main/java/com/discord/simpleast/code/CodeRules.kt
+++ simpleast-core/src/main/java/com/discord/simpleast/code/CodeRules.kt
@@ -234,6 +234,7 @@ object CodeRules {
 
         "py" to pythonRules,
         "python" to pythonRules,
+        "gyp" to pythonRules,
 
         "rs" to rustRules,
         "rust" to rustRules,
@@ -249,9 +250,13 @@ object CodeRules {
       
         "js" to javascriptRules,
         "javascript" to javascriptRules,
+        "jsx" to javascriptRules,
         
         "ts" to typescriptRules,
         "typescript" to typescriptRules,
+        "tsx" to typescriptRules,
+        "mts" to typescriptRules,
+        "cts" to typescriptRules,
 
         "patch" to diffRules,
         "diff" to diffRules
